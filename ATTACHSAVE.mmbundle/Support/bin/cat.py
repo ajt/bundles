5db49a7f361753a5aa9e6d77261ad3bea749d6c9
ajt <github@thorntonindustries.com>
2020-06-25 15:59:11 -0400

    Adding a new bundle for saving all attachments
    Untested

--- ATTACHSAVE.mmbundle/Support/bin/cat.py
+++ ATTACHSAVE.mmbundle/Support/bin/cat.py
@@ -12,12 +12,9 @@ log = open(log_file, 'w')
 # Parse the json from MailMate to save the attachments
 if "MM_FILES" in os.environ:
     for attachment in json.loads(os.environ['MM_FILES']):
-        if attachment["filePath"].lower().endswith(".pdf"):
+        if attachment["filePath"]:
             # Create directories for attachments, based on the sender name...
-            directory = os.environ['HOME'] + '/Attachments/' + os.environ['MM_FROM_NAME']
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-                log.write('Making directory: {}'.format(directory))
+            directory = '~/Library/Mobile\ Documents/com~apple~CloudDocs/potpourri/'
             log.write('Saving attachment: {}'.format(attachment))
             shutil.copy(attachment["filePath"], directory)
 
